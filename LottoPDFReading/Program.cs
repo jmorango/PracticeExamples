4c30644e497ce32b7bda2983ed3a92cff684dd4e
jmorango <joseamed@gmail.com>
2021-01-28 15:59:42 -0500

    adding a rectangle filter to the pdf reader

--- LottoPDFReading/Program.cs
+++ LottoPDFReading/Program.cs
@@ -3,6 +3,7 @@ using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using System.Text;
 
+
 namespace LottoPDFReading
 {
     class Program
@@ -17,18 +18,24 @@ namespace LottoPDFReading
         static private void getTextFromPDF()
         {
             
+            // TODO find out how to used the rectangle filter , ITextExtractionStrategy . 
+            // Adjust the numbers on the rectangle, a little more on the upper x,y 
+            var rect = new System.util.RectangleJ(18,36,125,620);
+            RenderFilter filter = new RegionTextRenderFilter(rect);
             StringBuilder text = new StringBuilder();
             using (PdfReader reader = new PdfReader("l6.pdf"))
             {
                 for (int i =1 ; i <= reader.NumberOfPages; i++)
                 {
-                    text.Append(PdfTextExtractor.GetTextFromPage(reader,i));
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader,i, new FilteredTextRenderListener(new LocationTextExtractionStrategy(),filter)));
                     
                 }
             }
             
-            int index = 0;
-            //todo parse string , eliminate empty spaces, correct eols 
+            
+            string output = text.ToString();
+             int index = 0;
+            
             while (index < text.Length - 1)
             {
                 char ch = text[index];
@@ -37,7 +44,7 @@ namespace LottoPDFReading
                 
                 if(ch=='\n' && Char.IsDigit(nextChar)||ch == 'X')
                 {
-                    Console.Write(' ');
+                    output += ' ';
                     index++;
                     continue;
                 }
@@ -48,10 +55,21 @@ namespace LottoPDFReading
                 }
                 if (Char.IsDigit(ch) || (ch ==' ' && Char.IsDigit(nextChar))  || ch =='/' ) 
                 {
-                   Console.Write(ch);
+                   output += ch;
                    
                 }
                 index++;
+            } 
+            using (System.IO.StreamWriter fileOut = new System.IO.StreamWriter(@"C:\Users\jmoran\source\repos\LottoPdfReading\output.txt"))
+            {
+                
+                fileOut.WriteLine(output);
+                foreach(string number in output.Split(' '))
+                {
+                    fileOut.Write(number);
+                }
+                
+                fileOut.Close();
             }
             
         }
